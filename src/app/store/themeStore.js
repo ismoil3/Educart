2e5f980c40113bd0605400019c54f0616048716b
ismoil3 <nasridinovismoil945@gmail.com>
2025-01-15 13:42:42 +0500

    edit store for localStorage

--- src/app/store/themeStore.js
+++ src/app/store/themeStore.js
@@ -1,15 +1,21 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const useThemeStore = create((set) => ({
   isDarkMode: (() => {
-    
-    const savedTheme = localStorage.getItem('isDarkMode');
-    return savedTheme !== null ? JSON.parse(savedTheme) : false;
+    // Check if `window` is defined to avoid issues in server-side rendering
+    if (typeof window !== "undefined") {
+      const savedTheme = localStorage.getItem("isDarkMode");
+      return savedTheme !== null ? JSON.parse(savedTheme) : false;
+    }
+    return false; // Default to false if no `window`
   })(),
   toggleTheme: () =>
     set((state) => {
-      const newMode = !state.isDarkMode; 
-      localStorage.setItem('isDarkMode', newMode); 
+      const newMode = !state.isDarkMode;
+      // Check if `window` is defined before accessing `localStorage`
+      if (typeof window !== "undefined") {
+        localStorage.setItem("isDarkMode", newMode);
+      }
       return { isDarkMode: newMode };
     }),
 }));
